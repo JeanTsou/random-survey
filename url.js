7523f03223e8b876f76cfb37b8ec245ff19c81bb
JeanTsou <95958828+JeanTsou@users.noreply.github.com>
2022-04-14 21:01:33 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,6 +8,16 @@
 
 
 const urls = [
+	'https://forms.gle/Dqqr6unqTMnFsAiK7',
+	'https://forms.gle/9GTZWDxhRCrTRLwz9',
+	'https://forms.gle/eBLRxhDynSpsbpcd7',
+	'https://forms.gle/hMVWTKahHn81jRRR9',
+	'https://forms.gle/HG6LYAeEdENYxs7c8',
+	'https://forms.gle/zD6HMkFJJ6M28d1H9',
+	'https://forms.gle/aBCXcyFm9t43qNCR7',
+	'https://forms.gle/XAwqerc1hs4RRFHd9',
+	'https://forms.gle/ZjxftLJUQZ9e4rZf9',
+	'https://forms.gle/dC5G63ysURsxfFdj6',
 	'https://forms.gle/mC7AJ6vBR7Bnd7yz6',
 	'https://forms.gle/NyFmZrpbYRN9ZBcSA'
 ];
